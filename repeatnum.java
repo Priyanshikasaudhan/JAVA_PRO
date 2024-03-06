5ad6e420810d47d7a455087992590ee446f3e661
Priyanshi <priyanshikasaudhan8429@gmail.com>
2024-03-06 11:01:45 +0530

    changes

--- repeatnum.java
+++ repeatnum.java
@@ -1,21 +1,30 @@
+import java.util.Scanner;
+
 public class repeatnum {
-    public static void Reapeatvalue(int arr[]){
-        for(int i=0;i<arr.length;i++){
-            if ((arr[i] != arr[i])) {
-                 System.out.println(false);
-            } 
-             if (arr[i] == arr[i]) {
-                System.out.println(true);
-             }
-           else{
-            System.out.println("Not match");
-           }     
-            }
+    public static boolean Reapeatvalue(int arr[]){
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if (arr[i] == arr[j]) {
+                    //  System.out.println(true);
+                    return true;
+            }  
         }
+    }
+    // System.out.println("Not Match.");
+        return false;
+    }
     
     public static void main(String[] args){
-        int arr[] = {1,1,2,4,5};
+        // int arr[] = {1,2,4,5};
         // System.out.println(Reapeatvalue(arr));
-        Reapeatvalue(arr);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.print("Enter the value: ");
+        for(int i=0;i<n;i++){
+         arr[i] = sc.nextInt();
+        }
+        System.out.println( Reapeatvalue(arr));
     }
 }
